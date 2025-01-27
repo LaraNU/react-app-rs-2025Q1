e3a061e75da480d006354f4df17ff3b034256a07
Larisa Novozhilova <kara4516@gmail.com>
2025-01-27 15:16:18 +0300

    fix: api request

--- src/api/apiService.ts
+++ src/api/apiService.ts
@@ -1,13 +1,61 @@
-const urlSearchParamsArts = new URLSearchParams({
-  fields: 'id,title,image_id,artist_title,date_display,place_of_origin',
-  page: `${1}`,
-  limit: `${12}`,
-});
+const BASE_URL = 'https://api.artic.edu/api/v1/artworks/search';
 
-export const getArtworks = async () => {
-  const response = await fetch(
-    `https://api.artic.edu/api/v1/artworks/search?q=Claude%20Monet&${urlSearchParamsArts}`
-  );
-  const artworks = await response.json();
-  return artworks.data;
+type QueryPart =
+  | { term: Record<string, string | number | boolean> }
+  | { wildcard: { title: string } };
+
+interface QueryParams {
+  query: {
+    bool: {
+      must: QueryPart[];
+    };
+  };
+  fields: string;
+  limit: number;
+  page: number;
+}
+
+const buildParams = (title?: string) => {
+  const query: QueryParams = {
+    query: {
+      bool: {
+        must: [
+          { term: { is_public_domain: true } },
+          {
+            term: { artist_id: 35809 },
+          },
+        ],
+      },
+    },
+    fields: `id,title,image_id,artist_title,date_display,place_of_origin`,
+    limit: 12,
+    page: 1,
+  };
+
+  if (title) {
+    query.query.bool.must.push({
+      wildcard: { title: `*${title}*` },
+    });
+  }
+
+  return encodeURIComponent(JSON.stringify(query));
+};
+
+export const fetchArtworks = async (title?: string) => {
+  const params = buildParams(title);
+
+  try {
+    const response = await fetch(`${BASE_URL}?params=${params}`);
+
+    if (!response.ok) {
+      throw new Error(`Ошибка: ${response.status}`);
+    }
+
+    const result = await response.json();
+
+    return result.data;
+  } catch (error) {
+    console.error('Error fetching artworks:', error);
+    return [];
+  }
 };
