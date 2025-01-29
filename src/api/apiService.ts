8ade3579129484c25091113cdfbaa272abb1813d
Larisa Novozhilova <kara4516@gmail.com>
2025-01-29 19:06:46 +0300

    feat: creat function for get image url

--- src/api/apiService.ts
+++ src/api/apiService.ts
@@ -60,3 +60,8 @@ export const fetchArtworks = async (title?: string) => {
     return [];
   }
 };
+
+export const getImageUrl = (id: string, size: string) => {
+  const IMAGE_BASE_URL = 'https://www.artic.edu/iiif/2/';
+  return `${IMAGE_BASE_URL}${id}/full/${size},/0/default.jpg`;
+};
