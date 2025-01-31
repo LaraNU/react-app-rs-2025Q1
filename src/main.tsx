d5f24d031864addec5db9b7f530d391270bf19b9
Larisa Novozhilova <kara4516@gmail.com>
2025-01-31 19:17:38 +0300

    refactor: remove comments eslint

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,15 @@ import './index.css';
 import { App } from './App.tsx';
 import { ErrorBoundary } from './components/ErrorBoundary/ErrorBoundary.tsx';
 
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <ErrorBoundary>
-      <App />
-    </ErrorBoundary>
-  </StrictMode>
-);
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  createRoot(rootElement).render(
+    <StrictMode>
+      <ErrorBoundary>
+        <App />
+      </ErrorBoundary>
+    </StrictMode>
+  );
+} else {
+  console.error('No root element found');
+}
