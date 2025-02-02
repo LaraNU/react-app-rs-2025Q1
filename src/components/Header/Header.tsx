fead40174c3b2589b89eb6764a5c4dcbedede6b2
Larisa Novozhilova <kara4516@gmail.com>
2025-02-02 14:55:34 +0300

    feat: changed Header component to functional components

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,22 +1,19 @@
 import styles from './Header.module.css';
 import logo from '../../assets/logo.png';
-import { Component, ReactNode } from 'react';
 import { Search } from '../Search/Search';
 
 type Props = {
   onSearch: (query: string) => void;
 };
 
-export class Header extends Component<Props> {
-  render(): ReactNode {
-    return (
-      <div className={styles.header}>
-        <div className={styles.logoWrapper}>
-          <img src={logo} alt="logo" />
-          <span className={styles.logoText}>Monet Art Explorer</span>
-        </div>
-        <Search onSearch={this.props.onSearch} />
+export const Header = ({ onSearch }: Props): React.JSX.Element => {
+  return (
+    <div className={styles.header}>
+      <div className={styles.logoWrapper}>
+        <img src={logo} alt="logo" />
+        <span className={styles.logoText}>Monet Art Explorer</span>
       </div>
-    );
-  }
-}
+      <Search onSearch={onSearch} />
+    </div>
+  );
+};
