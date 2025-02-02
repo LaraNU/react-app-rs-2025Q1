65605eb9369c3e578b044c7699c3b588b85a012a
Larisa Novozhilova <kara4516@gmail.com>
2025-02-02 14:58:52 +0300

    feat: changed Footer component to functional components

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,21 +1,18 @@
 import styles from './Footer.module.css';
 import logoGithub from '../../assets/icon-github.svg';
-import { Component, ReactNode } from 'react';
 
-export class Footer extends Component {
-  render(): ReactNode {
-    return (
-      <footer className={styles.footer}>
-        <div className={styles.text}>Monet Art Explorer</div>
-        <div className={styles.date}>2025</div>
-        <a
-          className={styles.link}
-          href="https://github.com/LaraNU"
-          target="_balnk"
-        >
-          <img src={logoGithub} alt="github" />
-        </a>
-      </footer>
-    );
-  }
-}
+export const Footer = (): React.JSX.Element => {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.text}>Monet Art Explorer</div>
+      <div className={styles.date}>2025</div>
+      <a
+        className={styles.link}
+        href="https://github.com/LaraNU"
+        target="_balnk"
+      >
+        <img src={logoGithub} alt="github" />
+      </a>
+    </footer>
+  );
+};
