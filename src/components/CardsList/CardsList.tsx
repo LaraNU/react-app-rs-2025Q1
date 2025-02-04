bcd0c8039e25ea21b055635bcff0f136cb41c702
Larisa Novozhilova <kara4516@gmail.com>
2025-02-04 19:33:00 +0300

    fix: added a check for isLoaded

--- src/components/CardsList/CardsList.tsx
+++ src/components/CardsList/CardsList.tsx
@@ -24,21 +24,23 @@ export const CardsList = (props: Props): React.JSX.Element => {
         </div>
       )}
 
-      {props.isSearchPerformed && props.artworks.length === 0 && (
-        <div className={styles.notFoundMsg}>
-          <p className={styles.textMsg}>
-            Sorry, we couldn&apos;t find any results for your search &#128577;
-          </p>
-          <p className={styles.textExp}>
-            Try searching for other famous artworks, such as:{' '}
-            <span>Bordighera,</span> <span>Water Lilies,</span>{' '}
-            <span>Cliff Walk at Pourville</span> and etc
-          </p>
-          <p className={styles.textExp}>
-            This might help you discover something interesting!
-          </p>
-        </div>
-      )}
+      {props.isSearchPerformed &&
+        props.artworks.length === 0 &&
+        props.isLoaded && (
+          <div className={styles.notFoundMsg}>
+            <p className={styles.textMsg}>
+              Sorry, we couldn&apos;t find any results for your search &#128577;
+            </p>
+            <p className={styles.textExp}>
+              Try searching for other famous artworks, such as:{' '}
+              <span>Bordighera,</span> <span>Water Lilies,</span>{' '}
+              <span>Cliff Walk at Pourville</span> and etc
+            </p>
+            <p className={styles.textExp}>
+              This might help you discover something interesting!
+            </p>
+          </div>
+        )}
 
       <ul className={styles.cardsList}>
         {!props.isLoaded && skeletonCards()}
