736657113b68a5d970366788669f5e4f8448b8cd
Larisa Novozhilova <kara4516@gmail.com>
2025-02-04 19:35:16 +0300

    feat: added a custom hook for storing data in LS

---
+++ src/utils/useQueryFromLS.ts
@@ -0,0 +1,32 @@
+import { useState, useEffect, Dispatch, SetStateAction } from 'react';
+
+export const useQueryFromLS = (
+  key: string,
+  initialValue: string
+): [
+  string,
+  Dispatch<SetStateAction<string>>,
+  boolean,
+  Dispatch<SetStateAction<boolean>>,
+] => {
+  const [searchValue, setSearchValue] = useState(() => {
+    const queryFromStorage = localStorage.getItem(key);
+    return queryFromStorage || initialValue;
+  });
+  const [searchPerformed, setSearchPerformed] = useState(() => {
+    return localStorage.getItem('searchPerformed') === 'true';
+  });
+
+  useEffect(() => {
+    localStorage.setItem(key, searchValue);
+    if (searchValue === '') {
+      localStorage.removeItem(key);
+    }
+  }, [key, searchValue]);
+
+  useEffect(() => {
+    localStorage.setItem('searchPerformed', String(searchPerformed));
+  }, [searchValue]);
+
+  return [searchValue, setSearchValue, searchPerformed, setSearchPerformed];
+};
