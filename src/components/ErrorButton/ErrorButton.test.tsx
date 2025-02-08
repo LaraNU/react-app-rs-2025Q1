fccf1e7010c4f50ffc2367b52a15b032ba84d731
Larisa Novozhilova <kara4516@gmail.com>
2025-02-08 12:03:27 +0300

    feat: add tests for error button

---
+++ src/components/ErrorButton/ErrorButton.test.tsx
@@ -0,0 +1,18 @@
+import { describe, expect, it } from 'vitest';
+import { fireEvent, render, screen } from '@testing-library/react';
+import { ErrorButton } from './ErrorButton';
+
+describe('test error button', () => {
+  it('error button renders correctly', () => {
+    render(<ErrorButton />);
+    expect(screen.getByText(/Error/i)).toBeDefined();
+  });
+
+  it('throws an error when clicked', () => {
+    expect(() => {
+      const button = screen.getByRole('button', { name: /error/i });
+
+      fireEvent.click(button);
+    }).toThrowError('Sorry.. there was an error');
+  });
+});
