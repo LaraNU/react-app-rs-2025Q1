9e15de6b418a2cbfd9f358a434e30ddebb49de90
Larisa Novozhilova <kara4516@gmail.com>
2025-02-08 18:16:50 +0300

    feat: add testing-library/user-event

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,8 +3,9 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     environment: 'jsdom',
+    globals: true,
     coverage: {
-      reporter: ['text', 'json', 'html'],
+      reporter: ['text'],
     },
     exclude: [
       '**/node_modules/**',
